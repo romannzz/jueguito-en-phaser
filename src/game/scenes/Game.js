9ff613a153cb01c6e746e76fe9dc75d04f6f0f0f
VictorRoman <r0m4nv1ct0r@gmail.com>
2025-05-19 21:13:33 +0200

    meti en preload todas nuestras imagenes dentro del apartado de game.js

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -13,6 +13,12 @@ export class Game extends Scene
         
         this.load.image('background', 'bg.png');
         this.load.image('logo', 'logo.png');
+
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
     }
 
     create ()
